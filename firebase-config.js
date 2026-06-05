635a2c131f77a6a6f9d2964c2435cc7e5dd95b0e
taszo <taszo@example.com>
2026-06-05 15:45:59 +0200

    تحديث إعدادات Firestore لربط قاعدة البيانات المخصصة باسم muhasib

--- firebase-config.js
+++ firebase-config.js
@@ -8,7 +8,8 @@ const firebaseConfig = {
     storageBucket: "muhasib-9cd3b.firebasestorage.app",
     messagingSenderId: "315392661607",
     appId: "1:315392661607:web:307ed256a2dc33907f1cc2",
-    measurementId: "G-6BGYG848R4"
+    measurementId: "G-6BGYG848R4",
+    databaseId: "muhasib" // اسم قاعدة البيانات المخصصة في Firestore
 };
 
 // تصدير الإعدادات للنافذة العامة لكي يستخدمها التطبيق
